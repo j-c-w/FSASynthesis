ad07278a2f5026ca66ce48740c60ffe654b77173
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-05-26 19:17:13 +0100

    Add ctags to project tools

--- shell.nix
+++ shell.nix
@@ -3,6 +3,6 @@
 with pkgs;
 
 mkShell {
-	buildInputs = [ python38 python38Packages.antlr4-python3-runtime antlr4 jdk gradle ];
+	buildInputs = [ python38 python38Packages.antlr4-python3-runtime antlr4 jdk gradle ctags ];
 	SHELL_NAME = "RegexSynthesis";
 }
