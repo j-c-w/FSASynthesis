555278e4064eaf17f13e05cf29b81d373485cd96
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-05-27 21:32:44 +0100

    Add some simple tests

---
+++ src/main/test/SimpleTests.java
@@ -0,0 +1,32 @@
+package main.test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import main.FSA.FSA;
+import main.Main;
+
+public class SimpleTests {
+	@Test
+	public void CheckCompileEquality() {
+		FSA result;
+		result = Main.compileSingleState("a", "b").generate();
+		assertNotEquals("Compilation must succeed", result, null);
+
+		result = Main.compileSingleState("a+", "[a-z]+").generate();
+		assertNotEquals("Compilation must succeed", result, null);
+
+		result = Main.compileSingleState("a*", "[a-z]+").generate();
+		assertEquals("Compilation must not succeed", result, null);
+
+		result = Main.compileSingleState("a*", "[a-z]+").generate();
+		assertEquals("Compilation must not succeed", result, null);
+
+		result = Main.compileSingleState("ab", "[a-z]").generate();
+		assertEquals("Compilation must not succeed", result, null);
+
+		result = Main.compileSingleState("ab", "[a-z]").generate();
+		assertEquals("Compilation must not succeed", result, null);
+	}
+}
